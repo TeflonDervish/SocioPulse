356065dfd63101a9c18128c133b14f4b676c6de7
Teflon Dervish <44845204+TeflonDervish@users.noreply.github.com>
2024-05-30 23:34:26 +0500

    make final design

--- app/src/main/java/com/avinash/sociopulse/CommentsActivity.kt
+++ app/src/main/java/com/avinash/sociopulse/CommentsActivity.kt
@@ -1,14 +1,21 @@
 package com.avinash.sociopulse
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.avinash.sociopulse.adapters.CommentsAdapter
 import com.avinash.sociopulse.databinding.ActivityCommentsBinding
 import com.avinash.sociopulse.models.Comment
+import com.avinash.sociopulse.models.Post
 import com.avinash.sociopulse.util.UserUtil
+import com.bumptech.glide.Glide
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.github.thunder413.datetimeutils.DateTimeStyle
+import com.github.thunder413.datetimeutils.DateTimeUtils
 import com.google.firebase.firestore.FirebaseFirestore
 
 class CommentsActivity : AppCompatActivity() {
@@ -19,6 +26,14 @@ class CommentsActivity : AppCompatActivity() {
     private var commentsAdapter: CommentsAdapter? = null
     private lateinit var recyclerView: RecyclerView
 
+    private lateinit var user_image: ImageView
+    private lateinit var post_author: TextView
+    private lateinit var post_text: TextView
+    private lateinit var post_time: TextView
+    private lateinit var post_image: ImageView
+    private val TAG = "StatisticActivity"
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCommentsBinding.inflate(layoutInflater)
@@ -27,9 +42,48 @@ class CommentsActivity : AppCompatActivity() {
         postId = intent.getStringExtra("postId")
         recyclerView = binding.commentsRecyclerView
 
+        db = FirebaseFirestore.getInstance();
 
+        postId = intent.getStringExtra("postId")
 
+        user_image = findViewById<ImageView>(R.id.user_image)
+        post_author = findViewById<TextView>(R.id.post_author)
+        post_text = findViewById<TextView>(R.id.post_text)
+        post_time = findViewById<TextView>(R.id.post_time)
+        post_image = findViewById<ImageView>(R.id.feed_post_image)
 
+        val postDoc = db.collection("Posts")
+            .document(postId!!).get().addOnCompleteListener { it ->
+                if (it.isSuccessful) {
+                    val post = it.result?.toObject(Post::class.java)
+
+                    val date = DateTimeUtils.formatDate(post!!.time)
+                    val dateFormatted = DateTimeUtils.formatWithStyle(date, DateTimeStyle.LONG)
+
+                    post_author.text = post!!.user.name
+                    post_text.text = post!!.text
+                    post_time.text = dateFormatted
+
+                    Glide.with(this)
+                        .load(post.imageUrl)
+                        .centerCrop()
+                        .placeholder(R.drawable.placeholder_image)
+                        .into(post_image)
+
+                    Glide.with(this)
+                        .load(post.user.imageUrl)
+                        .centerCrop()
+                        .placeholder(R.drawable.person_icon_black)
+                        .into(user_image)
+
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Something went wrong! Please Try again.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
         setUpRecyclerView()
 
         binding.commentSendIcon.setOnClickListener {
